4ceb59a1a5cb0be18234f31ba49d9ac9355b7c27
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-02-19 19:15:18 -0300

    test: update counter test to use userEvent for button clicks

--- src/__tests__/exercise/03.js
+++ src/__tests__/exercise/03.js
@@ -2,10 +2,11 @@
 // http://localhost:3000/counter
 
 import * as React from 'react'
-import {render, fireEvent, screen} from '@testing-library/react'
+import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Counter from '../../components/counter'
 
-test('counter increments and decrements when the buttons are clicked', () => {
+test('counter increments and decrements when the buttons are clicked', async () => {
   render(<Counter />)
   const decrement = screen.getByRole('button', {
     name: /decrement/i,
@@ -16,8 +17,8 @@ test('counter increments and decrements when the buttons are clicked', () => {
   const message = screen.getByText(/current count/i)
 
   expect(message).toHaveTextContent('Current count: 0')
-  fireEvent.click(increment)
+  await userEvent.click(increment)
   expect(message).toHaveTextContent('Current count: 1')
-  fireEvent.click(decrement)
+  await userEvent.click(decrement)
   expect(message).toHaveTextContent('Current count: 0')
 })
