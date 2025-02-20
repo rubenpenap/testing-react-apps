2cd3ee7e2cfd9f8a94ae7ebb1ac5dd27a7f73e3a
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-02-19 21:38:10 -0300

    test: add handling for unknown server error in Login component

--- src/__tests__/exercise/05.js
+++ src/__tests__/exercise/05.js
@@ -6,6 +6,7 @@ import {render, waitForElementToBeRemoved, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import {build, fake} from '@jackfranklin/test-data-bot'
 import {handlers} from 'test/server-handlers'
+import {rest} from 'msw'
 import {setupServer} from 'msw/node'
 import Login from '../../components/login-submission'
 
@@ -44,4 +45,20 @@ test(`omitting the password result in an error`, async () => {
   expect(screen.getByRole('alert').textContent).toMatchInlineSnapshot(
     `"password required"`,
   )
+})
+
+test('unknown server error displays the error message', async () => {
+  const testErrorMessage = 'Oh no, something bad happened'
+  server.use(
+    rest.post(
+      'https://auth-provider.example.com/api/login',
+      async (req, res, ctx) => {
+        return res(ctx.status(500), ctx.json({message: testErrorMessage}))
+      },
+    ),
+  )
+  render(<Login />)
+  await userEvent.click(screen.getByRole('button', {name: /submit/i}))
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+  expect(screen.getByRole('alert')).toHaveTextContent(testErrorMessage)
 })
